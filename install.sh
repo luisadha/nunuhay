32747673c0c80a832c7ee905d9322ae59e28d751
Luis Adha <40796508+luisadha@users.noreply.github.com>
2024-06-28 01:09:18 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+NUNUHAY_ROOT="${0%/*}"
+
+install -m 755 "$NUNUHAY_ROOT/nunuhay" "/data/data/com.termux/files/usr/bin"
+install -m 755 "$NUNUHAY_ROOT/libnunuhay.py" "/data/data/com.termux/files/usr/bin"
