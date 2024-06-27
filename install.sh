c1e8a5f1c66d02845ae53dcbb5209d5882d12e57
Luis Adha <40796508+luisadha@users.noreply.github.com>
2024-06-28 02:17:55 +0700

    bash ./install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+# bash ./install.sh
+
+set -xv
 NUNUHAY_ROOT="${0%/*}"
 
 mkdir -p /data/data/com.termux/files/home/.shortcuts
 chmod 700 -R /data/data/com.termux/files/home/.shortcuts
-cp -f "$NUNUHAY_ROOT/.shortcuts/*" "/data/data/com.termux/files/home/.shortcuts"
+
+install -m 755 "$NUNUHAY_ROOT/.shortcuts/Jalankan_server" "/data/data/com.termux/files/home/.shortcuts"
+install -m 755 "$NUNUHAY_ROOT/.shortcuts/Ubah_data_barang" "/data/data/com.termux/files/home/.shortcuts"
 install -m 755 "$NUNUHAY_ROOT/nunuhay" "/data/data/com.termux/files/usr/bin"
 install -m 755 "$NUNUHAY_ROOT/libnunuhay.py" "/data/data/com.termux/files/usr/bin"
 
