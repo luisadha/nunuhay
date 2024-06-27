78a805ee60a478b54137684634c01f2f82c15cdb
Luis Adha <40796508+luisadha@users.noreply.github.com>
2024-06-28 01:19:35 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 NUNUHAY_ROOT="${0%/*}"
 
+mkdir -p /data/data/com.termux/files/home/.shortcuts
+chmod 700 -R /data/data/com.termux/files/home/.shortcuts
+cp -f "$NUNUHAY_ROOT/.shortcuts/*" "/data/data/com.termux/files/home/.shortcuts"
 install -m 755 "$NUNUHAY_ROOT/nunuhay" "/data/data/com.termux/files/usr/bin"
 install -m 755 "$NUNUHAY_ROOT/libnunuhay.py" "/data/data/com.termux/files/usr/bin"
+
